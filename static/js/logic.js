2af0cca36abb911af729d67e48d8a33c0531ab04
unknown <piffardb@gmail.com>
2022-08-22 10:48:39 -0700

    Adding layers to a map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,18 +1,38 @@
-// Initializing the map, center of earth
-let myMap = L.map('mapid').setView([30, 30], 2);
-
-// Setting the tileLayer
+// Setting the tileLayer for streets map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/navigation-night-v1',
+    id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(myMap);
+//Setting the tileLayer for dark mode map
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// Adding basse layer to hold both map options
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+// Initializing the map, center of earth
+let myMap = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+
+// Adding base layers to layer control, and adding that to the main map
+L.control.layers(baseMaps).addTo(myMap);
 
 // Adding geoJSON data
 // Setting url
