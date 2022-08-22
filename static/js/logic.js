372336eafc0ff0fa618526f775472d7e7e847b8f
unknown <piffardb@gmail.com>
2022-08-21 18:47:17 -0700

    Adding files that create circle markers on map from an array of data

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,7 +5,7 @@ let myMap = L.map('mapid').setView([40.7, -94.5], 4);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -13,3 +13,17 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(myMap);
+
+let cityData = cities;
+
+// Adding a marker for each city
+cityData.forEach(city =>
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: '#dccce0',
+        fillColor: '#d957fa',
+        weight: 4
+    })
+    .bindPopup('<h2>' + city.city + ', ' + city.state + '</h2> <hr> <h3>Population: ' + city.population.toLocaleString() + '</h3>')
+    .addTo(myMap)
+);
