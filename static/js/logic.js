12c6be6b7fff92eaf9671246a812e2de7df191b1
unknown <piffardb@gmail.com>
2022-08-21 19:04:31 -0700

    Making a map with a line and dashes

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,11 @@
 // Initializing the map
-let myMap = L.map('mapid').setView([40.7, -94.5], 4);
+let myMap = L.map('mapid').setView([39.8283, -98.5795], 5);
 
 // Setting the tileLayer
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -14,16 +14,17 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(myMap);
 
-let cityData = cities;
+//let cityData = cities;
 
-// Adding a marker for each city
-cityData.forEach(city =>
-    L.circleMarker(city.location, {
-        radius: city.population/100000,
-        color: '#dccce0',
-        fillColor: '#d957fa',
-        weight: 4
-    })
-    .bindPopup('<h2>' + city.city + ', ' + city.state + '</h2> <hr> <h3>Population: ' + city.population.toLocaleString() + '</h3>')
-    .addTo(myMap)
-);
+// Adding a line on the map
+let line = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+];
+
+L.polyline(line, {
+    color: 'blue',
+    dashArray: "10"
+}).addTo(myMap);
